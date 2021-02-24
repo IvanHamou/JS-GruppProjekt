666b25c2a7299802c042fa41e5ba08ea6cc3ffd7
Alan Weik <alaan.m@hotmail.com>
2021-02-24 15:29:03 +0100

    Updated comments.

--- JS/fetch.js
+++ JS/fetch.js
@@ -1,3 +1,4 @@
+// Här är fetch-funktionen.
 async function fetchNorris() {
   const urlToFetch = "https://api.chucknorris.io/jokes/random";
   const response = await fetch(urlToFetch);
@@ -5,8 +6,8 @@ async function fetchNorris() {
   console.log(norrisData);
   return norrisData;
 }
-
+// Här kallar vi på vår fetch-funktion.
 fetchNorris();
-
+// Här ger vi våra HTML-element variabler.
 const $submit = $("#button");
 const $factDiv = $("#fact1");
--- JS/printer.js
+++ JS/printer.js
@@ -1,7 +1,8 @@
+// Här plockar vi ut önskad data ifrån API-fetch assignar datan till en variabel.
 const renderNorris = (values) => {
   $factDiv.each(($fact) => {
     const value = values.value;
   });
-
+  // Här stoppar vi in vår data i vår factDiv och skriver ut den i en H2.
   $factDiv.append(`<h2>${values.value}</h2>`);
 };
