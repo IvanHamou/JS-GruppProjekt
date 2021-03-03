a2f7480bef29c8d8c4124a0903f3a679dbae21c9
Ivan Hamou <ivanhamou@gmail.com>
2021-03-03 19:01:14 +0100

    display animations

--- JS/printer.js
+++ JS/printer.js
@@ -3,6 +3,16 @@ const renderNorris = (values) => {
   $factDiv.each(($fact) => {
     const value = values.value;
   });
-  // Här stoppar vi in vår data i vår factDiv och skriver ut den i en H2.
-  $factDiv.append(`<h2>${values.value}</h2>`);
+
+   
+  // Här stoppar vi in vår data i vår factDiv och skriver ut den i en H2 + animationer.
+
+  $factDiv.append(`<h2>${values.value}</h2>`)
+  .removeAttr("style")
+  .animate({letterSpacing: "+=5px"})
+  .animate({marginTop: "+=150px"}, 1000, "linear")
+  
+  
+  
 };
+
